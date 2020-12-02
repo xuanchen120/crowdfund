446c8ee0aaa5fadd6424cfe637d2aaf5649a8071
xuanchen <122383162@qq.com>
2020-12-02 13:22:04 +0800

    修改

--- config/crowdfund.php
+++ config/crowdfund.php
@@ -3,4 +3,17 @@
 return [
     'userModel'  => 'App\Models\User::class',
     'storeModel' => 'App\Models\Store::class',
+    
+    //后台管理路由
+    'routers'    => [
+        'admin' => [
+            'crowdfunds' => 'crowdfunds',
+        ],
+        'api'   => [
+            'crowdfunds' => 'crowdfunds',
+        ],
+        'agent' => [
+
+        ],
+    ],
 ];
